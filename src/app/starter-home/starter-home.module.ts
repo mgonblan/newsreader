50c553e2d1dc268599bcbb487e83a822d557380f
manana <manuel.gonzalez.freelancer@gmail.com>
2023-10-26 09:37:32 +0200

    Mensaje de subida

--- src/app/starter-home/starter-home.module.ts
+++ src/app/starter-home/starter-home.module.ts
@@ -5,7 +5,7 @@ import { StarterHomeRoutingModule } from './starter-home-routing.module';
 import { StarterHomeComponent } from './starter-home/starter-home.component';
 import { NotfoundComponent } from './notfound/notfound.component';
 import { BuilderModule } from "@builder.io/angular";
-import { environment } from "../../environments/environment.ts";
+import { environment } from "../../environments/environment";
 import { LoadingModule } from 'carbon-components-angular';
 @NgModule({
   declarations: [StarterHomeComponent, NotfoundComponent],
